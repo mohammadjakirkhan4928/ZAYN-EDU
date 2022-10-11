c4874e83b558edfb19ad26f179040e367bb7ab41
mdjakirkhan4928@gmail.com <mdjakirkhan4928@gmail.com>
2022-10-12 00:28:50 +0600

    color change

--- src/Component/Errorpage/Errorpage.js
+++ src/Component/Errorpage/Errorpage.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Errorpage = () => {
     return (
         <div>
-             <section className='flex items-center h-screen p-16 bg-salte-100 text-gray-900'>
+             <section className='flex items-center h-screen p-16 bg-gray-100 text-gray-700'>
       <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
         <svg
           xmlns='http://www.w3.org/2000/svg'
