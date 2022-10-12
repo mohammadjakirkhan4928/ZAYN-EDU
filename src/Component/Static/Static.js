31d42ae75ac32bde1ed3b75ba8dcb120805d0b33
mdjakirkhan4928@gmail.com <mdjakirkhan4928@gmail.com>
2022-10-12 23:48:38 +0600

    final

--- src/Component/Static/Static.js
+++ src/Component/Static/Static.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {  Line, XAxis, YAxis, Tooltip, ResponsiveContainer, ComposedChart, CartesianGrid, Legend, Area, Bar, Scatter } from 'recharts';
-
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 
 const Static = () => {
     const data = [
@@ -32,29 +31,14 @@ const Static = () => {
     return (
         <div>
               <h1>ReChart here</h1>
-              <ResponsiveContainer width="100%" height="100%">
-        <ComposedChart
-          width={500}
-          height={400}
-          data={data}
-          margin={{
-            top: 20,
-            right: 20,
-            bottom: 20,
-            left: 20,
-          }}
-        >
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="name" scale="band" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Area type="monotone" dataKey="amt" fill="#8884d8" stroke="#8884d8" />
-          <Bar dataKey="quiz" barSize={20} fill="#413ea0" />
-          <Line type="monotone" dataKey="total" stroke="#ff7300" />
-          <Scatter dataKey="cnt" fill="red" />
-        </ComposedChart>
-      </ResponsiveContainer>
+              <ResponsiveContainer className='mt-[200px] md:mt-[200px] sm:mt-[200px]' width={350} height={300}>
+                <BarChart width={500} height={400} data={data}>
+                    <Bar dataKey="total" fill="#8884d8" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip></Tooltip>
+                </BarChart>
+            </ResponsiveContainer>
         </div>
     );
 };
